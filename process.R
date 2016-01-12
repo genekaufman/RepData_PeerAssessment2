5dbb4a6cfa7f0bc306c8bb3fc1a41c941ff6f1f3
Gene Kaufman <genekaufman@gmail.com>
2016-01-12 17:03:25 -0500

    trying to reduce fields

--- process.R
+++ process.R
@@ -9,4 +9,18 @@ if (!file.exists(data_file_path)) {
 if (!exists("data_raw")) {
   data_raw <- read.csv(data_file_path)
 }
-#
+library(dplyr)
+# Many entries for EVTYPE, PROPDMGEXP and CROPDMGEXP are duplicated with different cases, set all to upper case
+data_raw$EVTYPE <- as.factor(toupper(data_raw$EVTYPE))
+data_raw$PROPDMGEXP <- as.factor(toupper(data_raw$PROPDMGEXP))
+data_raw$CROPDMGEXP <- as.factor(toupper(data_raw$CROPDMGEXP))
+
+wanted_fields <- c("BGN_DATE","STATE","EVTYPE","FATALITIES","INJURIES","PROPDMG","PROPDMGEXP","CROP","CROPDMGEXP")
+wanted_fields <- c("BGN_DATE","STATE","EVTYPE")
+data_pass1<- data_raw %>%
+  select(one_of(wanted_fields))
+
+# Population health = FATALITIES, INJURIES
+
+# economic consequences: PROPDMG, CROPDMG hold dollar amount, PROPDMGEXP, CROPDMGEXP
+# hold character showing magnitude of PROPDMG/CROPDMG (i.e. "B","H")
